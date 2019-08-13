25549c03a97dcca9c1a17ea6a559cd802712e191
Jharell Chambers <jharell.chambers@selu.edu>
2019-08-13 06:32:04 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,10 +1,18 @@
 def using_include(array, element)
+  array.include?(element)
 
 end
+ using_include([1,2,3,4,5], 4)
+#----------------------------------------
+
 
 def using_sort(array)
-
+ array.sort 
 end
+
+ using_sort([1,5,3,8,2])
+#----------------------------------------------
+
 
 def using_reverse(array)
 
